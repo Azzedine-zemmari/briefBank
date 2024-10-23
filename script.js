fab8896d432b0e1c954aa24a19a748f6e17ee064
Azzedine-zemmari <azzedinezemmari@gmail.com>
2024-10-23 10:34:59 +0100

    create a navigation responsive

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbar = document.getElementById("nav");
+const menu = document.getElementById("burger");
+
+function show() {
+    navbar.classList.toggle("show"); 
+}
+
+menu.addEventListener("click", show);
